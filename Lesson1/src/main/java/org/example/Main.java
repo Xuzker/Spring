d4524c889deff274153b529f7ede6582f0740be2
Xuzker <vyacheslav.gvozdikov@yandex.ru>
2025-10-06 10:54:40 +0300

    create MVC

---
+++ Lesson1/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Notifier email = new EmailNotifier();
+        UserService userService = new UserService(email);
+
+        userService.registerUser("Вячеслав");
+    }
+}
